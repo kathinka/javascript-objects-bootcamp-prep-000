969aa905c23c33cc51e3ffbaa5e7c1816b73b047
kathinka martinsen <kathinkamartinsen@gmail.com>
2018-09-02 17:35:53 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = {}
 
 function updatePlaylist(playlist, artist, song){
-  playlist ={ artist : song};
+  playlist[artist]= song;
   
   //object.assign ({}, playlist, {artist:song})
   return (playlist);

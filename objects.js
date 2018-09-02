72b333000eb36c42a1641fff55626b7bfa0575a4
kathinka martinsen <kathinkamartinsen@gmail.com>
2018-09-02 17:59:46 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,7 +3,7 @@ var artistName;
 var songTitle;
 
 function updatePlaylist(playlist, artistName, songTitle){
-  playlist[artistName]=songTitle;
+  playlist[artistName]=[songTitle];
   return playlist;
 }
 
